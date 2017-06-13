821bfd904b4ef4f8ee48bb1035b8012f6185de4f
tzdk <yefeng_dk@163.com>
2017-06-13 22:29:03 +0800

    add product

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module MasterRailsByActions
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.autoload_paths += %W[#{Rails.root}/lib]
     config.generators do |generator|
       generator.assets false
       generator.test_framework false
