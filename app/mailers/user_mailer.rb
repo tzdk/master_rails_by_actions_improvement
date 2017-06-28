fa58c4d8b05128e7caeb27e4d71d4f73dfdf17ed
tzdk <yefeng_dk@163.com>
2017-06-28 16:04:40 +0800

    heroku rename

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
 	def activation_needed_email(user)
 	  @user = user
-	  @url  = "http://localhost:3000/users/#{user.activation_token}/activate"
+	  @url  = "https://jing-bao.herokuapp.com//users/#{user.activation_token}/activate"
 	  delivery_options = { user_name: '529950736@qq.com',
                          password: 'gpnbgniasappbicg',
                          address: 'smtp.qq.com',
