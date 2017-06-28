c6995d02b364b8ed58ba69dcc3d26c4f3d2901ad
tzdk <yefeng_dk@163.com>
2017-06-28 15:13:33 +0800

    heroku deploy

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,8 @@ class UserMailer < ApplicationMailer
 	  delivery_options = { user_name: 'test_signup_2017@163.com',
                          password: 'sayyou200',
                          address: 'smtp.163.com',
-                         authentication: 'plain' }
+                         authentication: 'plain',
+                         enable_starttls_auto: true }
 	  mail(to: user.email, subject: 'Welcome to My Awesome Site',delivery_method_options: delivery_options)
 	end
 
